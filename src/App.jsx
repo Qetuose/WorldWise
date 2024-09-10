abb90f405470d4e7a4741c38dbc691fa070e7bc8
Qetuose <lukasbaltrimas123@gmail.com>
2024-09-11 01:35:17 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotfound from "./pages/PageNotfound";
+import Login from "./pages/Login";
+
 function App() {
-  const x = 23;
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+
+        <Route path="*" element={<PageNotfound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
