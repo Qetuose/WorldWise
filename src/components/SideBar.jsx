47a7d46b61934934744b7f8c9a4bdc121ec8f0c7
Qetuose <lukasbaltrimas123@gmail.com>
2024-09-11 17:31:17 +0300

    Routing

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
 function SideBar() {
-  return <div></div>;
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default SideBar;
